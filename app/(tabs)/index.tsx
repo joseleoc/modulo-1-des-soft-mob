7cd416f5969ef54ea22960dd412d40e808de30e1
Leonardo <joseleoc123@gmail.com>
2025-03-11 20:17:25 -0400

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,61 @@
+import { Image, StyleSheet, Platform } from "react-native";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/logo-eslaee-sin-fondo.png")}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Desarrollo de software móvil</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="title">Módulo 1</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Carrera que curso:</ThemedText>
+        <ThemedText>Intenieria informática</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Asignatura</ThemedText>
+        <ThemedText>Desarrollo de software móvil</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step Nombre del estudiante</ThemedText>
+        <ThemedText>José Leoanrdo Contreras Vargas</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Unidad</ThemedText>
+        <ThemedText>1</ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 180,
+    width: 290,
+    margin: 16,
+    position: "absolute",
+  },
+});
